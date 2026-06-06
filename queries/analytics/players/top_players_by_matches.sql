0a56b8aa2481d896af4cbe077c92fcac59b3fe95
Daniel Nascimento <daniel.nascimento9403@gmail.com>
2026-06-06 19:45:48 -0300

    Add player analytics queries, schema exploration and database joins

--- queries/analytics/players/top_players_by_matches.sql
+++ queries/analytics/players/top_players_by_matches.sql
@@ -1,9 +1,9 @@
 SELECT
-    u.username,
+    u.id AS user_id,
     COUNT(*) AS total_matches
 FROM match_player_stats mps
 INNER JOIN users u
     ON mps.user_id = u.id
-GROUP BY u.username
+GROUP BY u.id
 ORDER BY total_matches DESC
 LIMIT 10;
--- queries/analytics/players/users.sql
+++ queries/analytics/players/users.sql
@@ -1,5 +1,14 @@
 SELECT
-    COLUMN_NAME,
-    DATA_TYPE
-FROM INFORMATION_SCHEMA.COLUMNS
-WHERE TABLE_NAME = 'users';
+    COALESCE(
+        u.display_name,
+        u.cs2_nickname,
+        u.steam_persona_name,
+        u.id
+    ) AS player_name,
+    COUNT(*) AS total_matches
+FROM match_player_stats mps
+INNER JOIN users u
+    ON mps.user_id = u.id
+GROUP BY player_name
+ORDER BY total_matches DESC
+LIMIT 10;
